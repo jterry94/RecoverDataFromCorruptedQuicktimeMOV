9b8b2d806ac152325e6703fcfc67d502f4976b28
Jeff Terry <terryj@iit.edu>
2024-04-09 16:44:44 -0500

    Initial Commit

---
+++ RecoverQuicktime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RecoverQuicktime
+//
+//  Created by Jeff Terry on 4/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RecoverQuicktime/RecoverQuicktimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecoverQuicktimeApp.swift
+//  RecoverQuicktime
+//
+//  Created by Jeff Terry on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RecoverQuicktimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
